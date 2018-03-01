6be78854a272108b4f4f655f06d1907768f0f318
Mario Fink <mario.fink@goodyear.com>
2018-03-01 20:53:01 +0100

    add more videogame-themed examples

--- vagrant/catalog/addExampleData.py
+++ vagrant/catalog/addExampleData.py
@@ -2,16 +2,46 @@ from database_setup import Category, User
 import databaseService as db
 import productService
 
-category1 = Category(name="My first category")
-db.session.add(category1)
+snes = Category(name="Super Nintendo")
+db.session.add(snes)
+
+playstation = Category(name="Playstation")
+db.session.add(playstation)
+
+pc = Category(name="PC")
+db.session.add(pc)
+
 db.session.commit()
 
-product1 = {
-    'name': 'My first product',
-    'description': 'Some nice description',
-    'category': category1.id
-}
-productService.create(product1)
+productService.create({
+    'name': 'Super Mario World',
+    'description': 'Best platformer ever!',
+    'category': snes.id
+})
+
+productService.create({
+    'name': "Donkey Kong Country",
+    "description": "Go bananas",
+    "category": snes.id
+})
+
+productService.create({
+    "name": "NBA Jam",
+    "description": "He's on fire!",
+    "category": snes.id
+})
+
+productService.create({
+    "name": "Tomb Raider",
+    "description": "Dr. Jones? Never heard of him.",
+    "category": playstation.id
+})
+
+productService.create({
+    "name": "Populous",
+    "description": "The original God-game",
+    "category": pc.id
+})
 
 user1 = User(userid="admin", name="Administrator", password="nimda")
 db.session.add(user1)
