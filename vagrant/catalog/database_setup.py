beb847268a52a19cffa9ed69226038fb185f197c
Mario Fink <mario.fink@goodyear.com>
2018-03-01 10:35:32 +0100

    add database setup for products, categories and users

---
+++ vagrant/catalog/database_setup.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250))
+
+
+class Product(Base):
+    __tablename__ = 'product'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(Text)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(String(250), primary_key=True)
+    name = Column(String(250), nullable=False)
+    password = Column(Text, nullable=False)
+
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.create_all(engine)
