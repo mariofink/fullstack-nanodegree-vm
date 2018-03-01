095ef792b9fb15a9ef55ed622bf475f4746e7277
Mario Fink <mario.fink@goodyear.com>
2018-03-01 17:43:43 +0100

    use product service

--- vagrant/catalog/addExampleData.py
+++ vagrant/catalog/addExampleData.py
@@ -1,15 +1,17 @@
-from database_setup import Product, Category, User
-import datetime
+from database_setup import Category, User
 import databaseService as db
+import productService
 
 category1 = Category(name="My first category")
 db.session.add(category1)
 db.session.commit()
 
-product1 = Product(created=datetime.datetime.now(), name="My first product", description="Some nice description",
-                   category=category1)
-db.session.add(product1)
-db.session.commit()
+product1 = {
+    'name': 'My first product',
+    'description': 'Some nice description',
+    'category': category1.id
+}
+productService.create(product1)
 
 user1 = User(userid="admin", name="Administrator", password="nimda")
 db.session.add(user1)
